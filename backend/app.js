8e60f74c04d5a902f46cb01fde6f9f44719e8e7e
Gauri Shirke <gaurishirke.2005@gmail.com>
2025-06-04 17:51:04 +0530

    add-file

---
+++ backend/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const authRoutes = require('./routes/authRoutes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Welcome to TechHub Backend');
+});
+
+app.use('/api/auth', authRoutes);
+
+module.exports = app;
